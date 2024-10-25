d525972304473f323c038c3047f3c463ab9b51ec
hassamulhaq <developer.hassam@gmail.com>
2024-10-25 15:37:22 +0500

    Add basic form

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,88 @@
 <script setup lang="ts">
+
+import {reactive} from "vue";
+
+type UserData = {
+  "firstName": string,
+  "lastName": number,
+  "email": string,
+  "message": string
+}
+
+const formData = reactive<UserData>({});
+
+const onSubmitForm = () => {
+  console.log(formData);
+}
 </script>
 
 <template>
   <main>
-  <!-- <TheWelcome />-->
+    <!-- <TheWelcome />-->
 
+    <div class="flex items-center justify-center bg-stone-100 p-4 w-full">
+      <div class="p-6 rounded shadow-sm bg-stone-50 max-w-lg">
+        <div class="mb-6 p-6 bg-white">
+          <h1 class="font-bold text-2xl text-stone-700">Details</h1>
+        </div>
+        <form autocomplete="off" @submit.prevent="onSubmitForm">
+          <div class="grid grid-cols-2 gap-2.5">
+            <div class="flex flex-col mb-4">
+              <label>
+                <span>First Name</span>
+                <span class="block text-xs font-light text-stone-400">lorem ipsum details</span>
+              </label>
+              <input
+                type="text"
+                placeholder=""
+                class="mt-2 px-4 py-2 shadow rounded"
+                v-model:="formData.firstName"
+              />
+              <span class="block text-xs font-light text-red-800">First name is required!</span>
+            </div>
+            <div class="flex flex-col mb-4">
+              <label>
+                <span>Last Name</span>
+                <span class="block text-xs font-light text-stone-400">lorem ipsum details</span>
+              </label>
+              <input
+                type="text"
+                placeholder=""
+                class="mt-2 px-4 py-2 shadow rounded"
+                v-model:="formData.lastName"/>
+              <span class="block text-xs font-light text-red-800">Last name is required!</span>
+            </div>
+          </div>
+          <div class="flex flex-col mb-4">
+            <label>
+              <span>Email</span>
+              <span class="block text-xs font-light text-stone-400">lorem ipsum details</span>
+            </label>
+            <input
+              type="email"
+              placeholder=""
+              class="mt-2 px-4 py-2 shadow rounded"
+              v-model:="formData.email"/>
+            <span class="block text-xs font-light text-red-800">Email name is required!</span>
+          </div>
+          <div class="flex flex-col mb-4">
+            <label>
+              <span>Message</span>
+              <span class="block text-xs font-light text-stone-400">lorem ipsum details</span>
+            </label>
+            <textarea
+              rows="5"
+              placeholder=""
+              class="mt-2 px-6 py-2 shadow rounded"
+              v-model:="formData.message"></textarea>
+          </div>
+          <div class="mt-6 flex gap-6">
+            <button type="submit" class="rounded-full bg-orange-400 py-2 px-10 font-bold text-white shadow hover:bg-orange-500">
+              Button
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   </main>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  mode: 'jit',
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
